c1a671dbe02cd0cc4e8aa82f2fbc0e6fd874874c
d <d@lab.lab.com>
2024-05-12 01:23:52 -0400

    redirects correctly ignoring media paths to avoid nice hat

--- backToOld.js
+++ backToOld.js
@@ -1,8 +1,9 @@
 let currentUrlObject = window.location
 if (currentUrlObject){
 	let domain = currentUrlObject.hostname.split(".");
-	if (domain[0] != "old") {
-		let newUrl = `old.${domain.slice(1).join(".")}/${currentUrlObject.pathname}`
-		window.location.replace(newUrl)
+	if (domain[0] != "old" &&
+			currentUrlObject.pathname != "/media") {
+		let newUrl = `https://old.${domain.slice(1).join(".")}${currentUrlObject.pathname}${currentUrlObject.search}`	
+		window.location.href = newUrl
 	}
 }
